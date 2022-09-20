0c5bc7dfac06c28f5655e70852f59867f62934d4
GlenSHIHSHIH <a0935745150@gmail.com>
2022-09-20 15:50:38 +0800

    docker (laravel 9, nginx ,php8.0.23-fpm) init

---
+++ cdn_client/routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test/{id}', function (Request $request,$id) {
+    return phpinfo();
+});
+
+Route::post('/test/{id}', function (Request $request,$id) {
+    $data = $request->getContent();
+    $data = json_decode($data);
+    $data->id=$id;
+    // dd($data);
+    return response()->json($data);
+});
+
+Route::post('/testjson', function (Request $request) {
+    $data= $request->all();
+    // dd($data);
+    return response()->json($data);
+});
