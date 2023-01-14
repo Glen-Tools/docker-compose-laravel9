d6dd297f62d345113447437ae3695c69ae0dff41
GlenSHIHSHIH <a0935745150@gmail.com>
2023-01-14 12:26:12 +0800

    test nginx remote ip problem

--- cdn_client/app/Http/Controllers/TestController.php
+++ cdn_client/app/Http/Controllers/TestController.php
@@ -40,9 +40,15 @@ class TestController extends BaseController
 
     public function test(Request $request)
     {
-        $route = $request->route();
-        $actionName = explode('\\', $route->getActionName());
-        $controllerMethod = $actionName[count($actionName) - 1];
-        var_dump($controllerMethod);
+
+        var_dump((!empty($_SERVER["proxy_add_x_forwarded_for"])) ? "proxy_add_x_forwarded_for:" . $_SERVER["proxy_add_x_forwarded_for"] : "proxy_add_x_forwarded_for:empty");
+        var_dump((!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) ? "HTTP_X_FORWARDED_FOR:" . $_SERVER["HTTP_X_FORWARDED_FOR"] : "HTTP_X_FORWARDED_FOR:empty");
+        var_dump((!empty($_SERVER["HTTP_CLIENT_IP"])) ? "HTTP_CLIENT_IP:" . $_SERVER["HTTP_CLIENT_IP"] : "HTTP_CLIENT_IP:empty");
+        var_dump((!empty($_SERVER["REMOTE_ADDR"])) ? "REMOTE_ADDR:" . $_SERVER["REMOTE_ADDR"] : "REMOTE_ADDR:empty");
+        var_dump((!empty(getenv('HTTP_X_FORWARDED_FOR'))) ? "HTTP_X_FORWARDED_FOR:" . (getenv('HTTP_X_FORWARDED_FOR')) : "HTTP_X_FORWARDED_FOR:empty");
+        var_dump((!empty(getenv('HTTP_CLIENT_IP'))) ? "HTTP_CLIENT_IP:" . (getenv('HTTP_CLIENT_IP')) : "HTTP_CLIENT_IP:empty");
+        var_dump((!empty(getenv('REMOTE_ADDR'))) ? "REMOTE_ADDR:" . (getenv('REMOTE_ADDR')) : "REMOTE_ADDR:empty");
+        var_dump($request->getClientIp());
+        var_dump($request->ip());
     }
 }
