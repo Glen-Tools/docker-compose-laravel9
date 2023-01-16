7d923bea01886d76ae8c474e11225651af6c8295
GlenSHIHSHIH <a0935745150@gmail.com>
2023-01-16 16:23:36 +0800

    fix permissions problem

--- run_docker.sh
+++ run_docker.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-chattr -ai /etc/passwd
+# chattr -ai /etc/passwd
 
-chattr -ai /etc/shadow
+# chattr -ai /etc/shadow
 
-groupadd www
+# groupadd www
 
-useradd -g www -p www www 
+# useradd -g www -p www www 
 
-chown -R www:root cdn_client
+# chown -R 1000:1000 cdn_client
 
-chown -R www:root laravel_env_volume
+# chown -R www:root laravel_env_volume
+
+
 
 #cover env and run docker
 cp cdn_client/.env.production laravel_env_volume/.env
 
+chown -R 1000:1000 cdn_client db/config laravel_env_volume nginx php redis 
 
 docker-compose up -d
