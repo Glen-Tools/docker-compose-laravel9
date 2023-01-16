91ae6a0292921ecfc116dc29282151ebbab42539
GlenSHIHSHIH <a0935745150@gmail.com>
2023-01-16 15:12:21 +0800

    update docker-compose volume, nginx conf and run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+chattr -ai /etc/passwd
+
+chattr -ai /etc/shadow
+
+groupadd www
+
+useradd -g www -p www www 
+
+chown -r www:root cdn_client
+
+chown -r www:root laravel_env_volume
+
 #cover env and run docker
 cp cdn_client/.env.production laravel_env_volume/.env
 
